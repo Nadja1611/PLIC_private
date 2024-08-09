1a595f530a98a1afc9bca93e33a1a9429e56fef1
Nadja1611 <61620034+Nadja1611@users.noreply.github.com>
2024-08-09 09:50:07 +0200

    Update model_try.py

--- model_try.py
+++ model_try.py
@@ -1,9 +1,8 @@
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import Dataset,DataLoader
-from torchvision import utils,models
+from torch.utils.data import Dataset, DataLoader
+from torchvision import utils, models
 import torch.nn.functional as F
 import torchvision.transforms as T
 from sklearn.model_selection import train_test_split
@@ -23,28 +22,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import gc
 import argparse
-from functions_new import preprocessing_no_patching,  create_boxes_sag_cor, preprocessing_coronal_sagittal
+from functions_new import preprocessing_no_patching, create_boxes_sag_cor, preprocessing_coronal_sagittal
 from dataset import *
 
 
-
-
 ''' set argparse elements relevant for the training of the baby method '''
-parser = argparse.ArgumentParser(description='Arguments for segmentation network.',add_help=False)
-parser.add_argument('-l','--loss_variant', type=str, 
-                    help='which loss variant should be used', default= "Dice")
-parser.add_argument('-alpha','--alpha', type=float, 
-                    help='how much noisier should z be than y', default= 0.5)
-parser.add_argument('-lr','--learning_rate', type=str, 
-                    help='which learning rate should be used', default= 1e-5)
-parser.add_argument('-i', '--inputdir', type=str, 
-                    help='directory for input files', default = "/home/nadja/nadja/PLIC/Data_npz" )
-parser.add_argument('-batch_size', '--batch_size', type=int, 
-                    help='batch size used for dataloader', default = 1 )
-parser.add_argument('-N_training_data', '--N_training_data', type=int, 
-                    help='how many volumes are used for training', default = 85 )
-parser.add_argument('-N_augmentations', '--N_augmentations', type=int, 
-                    help='what is the factor of increased volumes because of data augmentations', default = 3 )
+parser = argparse.ArgumentParser(description='Arguments for segmentation network.', add_help=False)
+parser.add_argument('-l', '--loss_variant', type=str,
+                    help='which loss variant should be used', default="Dice")
+parser.add_argument('-alpha', '--alpha', type=float,
+                    help='how much noisier should z be than y', default=0.5)
+parser.add_argument('-lr', '--learning_rate', type=str,
+                    help='which learning rate should be used', default=1e-5)
+parser.add_argument('-i', '--inputdir', type=str,
+                    help='directory for input files', default="/home/nadja/nadja/PLIC/Data_npz")
+parser.add_argument('-batch_size', '--batch_size', type=int,
+                    help='batch size used for dataloader', default=1)
+parser.add_argument('-N_training_data', '--N_training_data', type=int,
+                    help='how many volumes are used for training', default=85)
+parser.add_argument('-N_augmentations', '--N_augmentations', type=int,
+                    help='what is the factor of increased volumes because of data augmentations', default=3)
 args = parser.parse_args()
 
 
@@ -61,9 +58,7 @@ def split_volumes(x_new, indices):
     return volumes
 
 
-
 device = "cuda"
-
 
 
 N_total = 98
@@ -102,31 +97,30 @@ class UNet(nn.Module):
         return logits
 
 
-
 class Segmentation_of_PLIC():
     def __init__(
         self,
         learning_rate: float = 5e-5,
         device: str = 'cuda:0',
         inputs: str = "T1",
-        sliceview: str= "axial"
+        sliceview: str = "axial"
     ):
-        self.mean_FP=[]
-        self.mean_FN=[]
-        self.Recall= 0
-        self.max_mean_dice=0
-        self.max_mean_dice_sag=0
-        self.max_mean_dice_cor=0
+        self.mean_FP = []
+        self.mean_FN = []
+        self.Recall = 0
+        self.max_mean_dice = 0
+        self.max_mean_dice_sag = 0
+        self.max_mean_dice_cor = 0
 
-        self.mean_recall=[]
+        self.mean_recall = []
         self.learning_rate = 5e-5
-        self.net = UNet(1,1)
+        self.net = UNet(1, 1)
         self.weight = 0.001
-        #self.inputs = "DWI"
-        self.segmentation_net = UNet(n_channels=1,n_classes=1).to(device)
-        self.segmentation_net_sag = UNet(n_channels=1,n_classes=1).to(device)
-        self.segmentation_net_cor = UNet(n_channels=1,n_classes=1).to(device)
-        
+        # self.inputs = "DWI"
+        self.segmentation_net = UNet(n_channels=1, n_classes=1).to(device)
+        self.segmentation_net_sag = UNet(n_channels=1, n_classes=1).to(device)
+        self.segmentation_net_cor = UNet(n_channels=1, n_classes=1).to(device)
+
         self.optimizer = optim.Adam(self.segmentation_net.parameters(), lr=self.learning_rate)
         self.optimizer_cor = optim.Adam(self.segmentation_net_cor.parameters(), lr=self.learning_rate)
         self.optimizer_sag = optim.Adam(self.segmentation_net_sag.parameters(), lr=self.learning_rate)
@@ -135,148 +129,184 @@ class Segmentation_of_PLIC():
         self.Dice = 0
         self.Dice_isles = 0
         self.inputs = inputs
-        self.mean_dice=[]
-        self.mean_dice_isles=[]
+        self.mean_dice = []
+        self.mean_dice_isles = []
         self.mean_dice_isles_sag = []
-        self.mean_dice_sag=[]
+        self.mean_dice_sag = []
         self.mean_dice_isles_cor = []
         self.mean_dice_cor = []
-        self.FP= 0
+        self.FP = 0
         self.FN = 0
-        self.mean_spec=[]
+        self.mean_spec = []
         self.Spec = 0
-        self.alpha=0.7
-        self.gamma=0.5
-        self.delta=0.5
-        self.data_val_sag=None
-        self.data_sag=None
+        self.alpha = 0.7
+        self.gamma = 0.5
+        self.delta = 0.5
+        self.data_val_sag = None
+        self.data_sag = None
         self.data_cor = None
-        self.data_val_cor=None
+        self.data_val_cor = None
         self.batch_size = args.batch_size
-        
+
         self.noise = "gauss"
         self.noise_intensity = 0.1
 
-
-                # Dataset
-        self.train_dataset = Babyloader(noise_type = self.noise, noise_intensity = self.noise_intensity, train=True)
-        self.test_dataset = Babyloader(noise_type = self.noise, noise_intensity = self.noise_intensity, train=False)
+        # Dataset
+        self.train_dataset = Babyloader(noise_type=self.noise, noise_intensity=self.noise_intensity, train=True)
+        self.test_dataset = Babyloader(noise_type=self.noise, noise_intensity=self.noise_intensity, train=False)
         self.train_dataloader = DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
         self.test_dataloader = DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False)
-        
+
+        # For tracking and plotting loss and metrics
+        self.train_losses = []
+        self.val_losses = []
+        self.val_dices = []
+
     def init_NW(self, device):
         if self.inputs == "T1":
-            self.segmentation_net = UNet(n_channels=1,n_classes=1).to(device)    
-            self.segmentation_net_cor = UNet(n_channels=1,n_classes=1).to(device)  
-            self.segmentation_sag = UNet(n_channels=1,n_classes=1).to(device)       
-
+            self.segmentation_net = UNet(n_channels=1, n_classes=1).to(device)
+            self.segmentation_net_cor = UNet(n_channels=1, n_classes=1).to(device)
+            self.segmentation_sag = UNet(n_channels=1, n_classes=1).to(device)
 
         self.optimizer = optim.Adam(self.segmentation_net.parameters(), lr=self.learning_rate)
         self.optimizer_cor = optim.Adam(self.segmentation_net_cor.parameters(), lr=self.learning_rate)
         self.optimizer_sag = optim.Adam(self.segmentation_net_sag.parameters(), lr=self.learning_rate)
 
     def tversky(self, tp, fn, fp):
-        loss2 = 1- ((torch.sum(tp)+0.000001)/((torch.sum(tp) + self.gamma*torch.sum(fn) + self.delta*torch.sum(fp)+0.000001)))
+        loss2 = 1 - ((torch.sum(tp) + 0.000001) / ((torch.sum(tp) + self.gamma * torch.sum(fn) + self.delta * torch.sum(fp) + 0.000001)))
         return loss2
 
-           
-    def dice_loss(self, segmentation_mask, output):        
-        weights = torch.stack([torch.tensor(1-self.weight), torch.tensor(self.weight)]).to(self.device)
-        output = torch.stack([output, 1-output], axis=-1)
-        segmentation_mask = torch.stack([segmentation_mask, 1-segmentation_mask], axis=-1)
+    def dice_loss(self, segmentation_mask, output):
+        weights = torch.stack([torch.tensor(1 - self.weight), torch.tensor(self.weight)]).to(self.device)
+        output = torch.stack([output, 1 - output], axis=-1)
+        segmentation_mask = torch.stack([segmentation_mask, 1 - segmentation_mask], axis=-1)
 
-       # weights = 1-torch.tensor(self.weight)
-        output = torch.clip(output, min = 1e-6)
-        loss1 = -torch.sum(segmentation_mask * torch.log(output)* weights,axis = -1)
+        output = torch.clip(output, min=1e-6)
+        loss1 = -torch.sum(segmentation_mask * torch.log(output) * weights, axis=-1)
         loss1 = torch.mean(loss1)
-        '''tversky preperation'''
-        y_true_f = torch.flatten(segmentation_mask[:,:,:,:])
-        y_pred_f = torch.flatten(output[:,:,:,:])
-        fp = (1-y_true_f)*y_pred_f
-        fn = (1-y_pred_f)*y_true_f
-        tp = y_pred_f*y_true_f
 
-        loss = (self.alpha*loss1) + (1-self.alpha)*(self.tversky(tp,fn,fp))
-        #loss = self.tversky(tp,fn,fp
+        '''tversky preperation'''
+        y_true_f = torch.flatten(segmentation_mask[:, :, :, :])
+        y_pred_f = torch.flatten(output[:, :, :, :])
+        fp = (1 - y_true_f) * y_pred_f
+        fn = (1 - y_pred_f) * y_true_f
+        tp = y_pred_f * y_true_f
+
+        loss = (self.alpha * loss1) + (1 - self.alpha) * (self.tversky(tp, fn, fp))
         del(tp, fp, fn, y_true_f, y_pred_f)
         gc.collect()
         return loss
 
+    def dice_coefficient(self, y_true, y_pred, epsilon=1e-6):
+        y_true_f = y_true.flatten()
+        y_pred_f = y_pred.flatten()
+        intersection = (y_true_f * y_pred_f).sum()
+        return (2. * intersection + epsilon) / (y_true_f.sum() + y_pred_f.sum() + epsilon)
 
 
 Segmenter = Segmentation_of_PLIC()
-for epoch in range(1):
+
+for epoch in range(10):  # Increase the number of epochs for more meaningful training
+    train_loss = 0.0
+    val_loss = 0.0
+    val_dice = 0.0
+    Segmenter.segmentation_net.train()
+    Segmenter.segmentation_net_cor.train()
+    Segmenter.segmentation_net_sag.train()
+
     for i, batch in enumerate(Segmenter.train_dataloader):
-        
-        Segmenter.segmentation_net.train()
-        Segmenter.segmentation_net_cor.train()
-        Segmenter.segmentation_net_sag.train()
 
+        # Zero the parameter gradients
+        Segmenter.optimizer.zero_grad()
+        Segmenter.optimizer_cor.zero_grad()
+        Segmenter.optimizer_sag.zero_grad()
 
-
-        ### read in the T1-cubes of size (1, number slices, 64,64)
+        # Load inputs and masks
         inputs = batch['data_patch'].float().squeeze().unsqueeze(1).to(device)
         mask = batch['mask_patch'].squeeze().unsqueeze(1).to(device)
-        T1 = batch["data"].squeeze()
         inputs_sag = batch['data_patch_sag'].float().squeeze().unsqueeze(1).to(device)
         inputs_cor = batch['data_patch_cor'].float().squeeze().unsqueeze(1).to(device)
         mask_sag = batch['mask_patch_sag'].float().squeeze().unsqueeze(1).to(device)
         mask_cor = batch['mask_patch_cor'].float().squeeze().unsqueeze(1).to(device)
 
-
-        if epoch % 1000 == 0:
-            with torch.no_grad():
-                plt.subplot(1,2,1)
-                plt.imshow(inputs[40][0].detach().cpu(), cmap='gray')
-                plt.subplot(1,2,2)
-                plt.imshow(mask[40][0].detach().cpu(), cmap='gray')
-                plt.show()
-                #plt.savefig(newpath + '/'+ "data" + ".png")
-                #plt.close()
-        Segmenter.optimizer.zero_grad()
-        Segmenter.optimizer_cor.zero_grad()
-        Segmenter.optimizer_sag.zero_grad()
-        
+        # Forward pass
         output = F.sigmoid(Segmenter.segmentation_net(inputs))
-        loss = Segmenter.dice_loss(mask, output)
-        
         output_cor = F.sigmoid(Segmenter.segmentation_net_cor(inputs_cor))
-        loss_cor = Segmenter.dice_loss(mask_cor, output_cor)
-        
         output_sag = F.sigmoid(Segmenter.segmentation_net_sag(inputs_sag))
+
+        # Compute loss
+        loss = Segmenter.dice_loss(mask, output)
+        loss_cor = Segmenter.dice_loss(mask_cor, output_cor)
         loss_sag = Segmenter.dice_loss(mask_sag, output_sag)
 
-
+        # Backward pass and optimization
         loss.backward()
         Segmenter.optimizer.step()
 
         loss_cor.backward()
         Segmenter.optimizer_cor.step()
-        
-        loss_sag.backwawrd()
+
+        loss_sag.backward()
         Segmenter.optimizer_sag.step()
-        
-        
-                
-        with torch.no_grad():
-        
-            Segmenter.segmentation_net.train()
-            Segmenter.segmentation_net_cor.train()
-            Segmenter.segmentation_net_sag.train()
 
-            for batch, data in enumerate(Segmenter.test_dataloader):
-        
-                ### read in the T1-cubes of size (1, number slices, 64,64)
-                inputs = batch['data_patch'].float().squeeze().unsqueeze(1).to(device)
-                mask = batch['mask_patch'].squeeze().unsqueeze(1).to(device)
-                T1 = batch["data"].squeeze()
-                inputs_sag = batch['data_patch_sag'].float().squeeze().unsqueeze(1).to(device)
-                inputs_cor = batch['data_patch_cor'].float().squeeze().unsqueeze(1).to(device)
-                mask_sag = batch['mask_patch_sag'].float().squeeze().unsqueeze(1).to(device)
-                mask_cor = batch['mask_patch_cor'].float().squeeze().unsqueeze(1).to(device)
+        train_loss += (loss.item() + loss_cor.item() + loss_sag.item())
 
-                output = F.sigmoid(Segmenter.segmentation_net(inputs))
-                
-                output_cor = F.sigmoid(Segmenter.segmentation_net_cor(inputs_cor))
-                
-                output_sag = F.sigmoid(Segmenter.segmentation_net_sag(inputs_sag))
+    Segmenter.train_losses.append(train_loss / len(Segmenter.train_dataloader))
+
+    with torch.no_grad():
+        Segmenter.segmentation_net.eval()
+        Segmenter.segmentation_net_cor.eval()
+        Segmenter.segmentation_net_sag.eval()
+
+        for i, batch in enumerate(Segmenter.test_dataloader):
+            # Load inputs and masks
+            inputs = batch['data_patch'].float().squeeze().unsqueeze(1).to(device)
+            mask = batch['mask_patch'].squeeze().unsqueeze(1).to(device)
+            inputs_sag = batch['data_patch_sag'].float().squeeze().unsqueeze(1).to(device)
+            inputs_cor = batch['data_patch_cor'].float().squeeze().unsqueeze(1).to(device)
+            mask_sag = batch['mask_patch_sag'].float().squeeze().unsqueeze(1).to(device)
+            mask_cor = batch['mask_patch_cor'].float().squeeze().unsqueeze(1).to(device)
+
+            # Forward pass
+            output = F.sigmoid(Segmenter.segmentation_net(inputs))
+            output_cor = F.sigmoid(Segmenter.segmentation_net_cor(inputs_cor))
+            output_sag = F.sigmoid(Segmenter.segmentation_net_sag(inputs_sag))
+
+            # Compute loss
+            loss = Segmenter.dice_loss(mask, output)
+            loss_cor = Segmenter.dice_loss(mask_cor, output_cor)
+            loss_sag = Segmenter.dice_loss(mask_sag, output_sag)
+
+            val_loss += (loss.item() + loss_cor.item() + loss_sag.item())
+
+            # Compute Dice coefficient
+            dice = Segmenter.dice_coefficient(mask, output)
+            dice_cor = Segmenter.dice_coefficient(mask_cor, output_cor)
+            dice_sag = Segmenter.dice_coefficient(mask_sag, output_sag)
+
+            val_dice += (dice.item() + dice_cor.item() + dice_sag.item())
+
+        Segmenter.val_losses.append(val_loss / len(Segmenter.test_dataloader))
+        Segmenter.val_dices.append(val_dice / len(Segmenter.test_dataloader))
+
+    print(f'Epoch {epoch + 1}/{10}, Training Loss: {Segmenter.train_losses[-1]:.4f}, Validation Loss: {Segmenter.val_losses[-1]:.4f}, Validation Dice: {Segmenter.val_dices[-1]:.4f}')
+
+
+# Plotting the results
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.plot(Segmenter.train_losses, label='Training Loss')
+plt.plot(Segmenter.val_losses, label='Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.title('Loss Plot')
+
+plt.subplot(1, 2, 2)
+plt.plot(Segmenter.val_dices, label='Validation Dice')
+plt.xlabel('Epochs')
+plt.ylabel('Dice Coefficient')
+plt.legend()
+plt.title('Dice Coefficient Plot')
+
+plt.show()
